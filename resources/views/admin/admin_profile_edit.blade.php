8622c628608e844a4293d8e30f57e977177a316a
unknown <armanhaussain@gmail.com>
2022-07-04 16:41:32 +0600

    edit profile section

---
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<div class="page-content">
+    <div class="container-fluid">
+
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="card-title">Edit Profile Page</h4>
+                        <form action="">
+                        <div class="row mb-3">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" type="text" name="name" value="{{$editData->name}}" id="example-text-input">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" type="email" name="email" value="{{$editData->email}}" id="example-text-input">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Username</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" type="text" name="username" value="{{$editData->username}}" id="example-text-input">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" type="file" name="profile_image" id="image">
+                            </div>
+                        </div>
+
+
+                        <div class="row mb-3">
+                            <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                            <div class="col-sm-10">
+                                <img id="showImage" class="card-img-top img-fluid" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap" style="width:200px" class="img-thumbnail">
+                            </div>
+                        </div>
+
+                        <input type="submit" value="Update Profile" class="btn btn-success">
+
+                        </form>
+
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result)
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
+
+@endsection
